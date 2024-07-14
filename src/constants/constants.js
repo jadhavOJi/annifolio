c8a8053a0204ac9e0bbe147754fa582b33fe2111
jadhavOJi <158740493+jadhavOJi@users.noreply.github.com>
2024-07-14 10:48:43 +0530

    c

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -161,155 +161,44 @@ const projects = [
     live_link: "https://www.hawkeyeforensic.com/",
   },
   {
-    name: "Hayasaka",
+    name: "flamingcolours",
     description:
-      "A dynamic full-stack music streaming web application with Next.js 13 and MongoDB.",
+      "A🥀 Love artist colours?🎨 , then it is a site for colour your artistic therapy✨",
     features: [
-      "User authentication using NextAuth.js with JWT and OAuth providers.",
-      "Redux to Manage app state effectively.",
-      "Play, pause, skip, and control the playback of songs.",
-      "Users can Create, update and delete their own playlists.",
-      "Add songs to favorites in one click.",
-      "Auto adds similar songs to the queue.",
-      "Swipe and touch gestures for touch-responsive devices.",
-      "Display song details such as title, artist, album, and album artwork.",
-      "PWA (Progressive Web App) support. ",
+      "_flamingcolours_ Paintings & Arts",
+      "Most Artistic works you have probably never seen before",
     ],
     tags: [
       {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
+        name: "Paintings",
         color: "pink-text-gradient",
       },
       {
-        name: "nextjs",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "nextauth",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "redux",
+        name: "Arts",
         color: "green-text-gradient",
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/himanshu8443/hayasaka",
-    live_link: "https://hayasaka.live/",
+    live_link: "https://flamingcolours.lovestoblog.com",
   },
   {
-    name: "Study Notion",
-    description: "Developed an educational technology website with CMS.",
+    name: "random_tasveerein_",
+    description: "🥀 Random snippets from a phone!",
     features: [
-      "Implemented Email verification and using OTP user authentication and login/signup functionality using JWT for secure access to the platform.",
-      "Students can browse and purchase courses. Instructors can create new courses, edit existing courses, and delete courses they have created.",
-      "Integrated MongoDB database to store user information, course data, and progress tracking.",
-      "Employed RESTful API principles for seamless communication between the frontend and backend systems.",
-      "Razorpay payment integration.",
-      "PWA (Progressive Web App) support.",
+      "Curated pictures of best sceneries i came across the road-trips....",
     ],
     tags: [
       {
-        name: "react",
+        name: "Photography",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "ExpressJs",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "nodejs",
+        name: "Videography",
         color: "green-text-gradient",
       },
     ],
     image: jobit,
-    source_code_link: "https://github.com/himanshu8443/Study-Notion-master",
     live_link: "https://studynotion-master.vercel.app/",
-  },
-  {
-    name: "FlipKart Price Tracker",
-    description:
-      "Chrome extension to track FlipKart product prices and send an alert to users when prices go below the user-selected price.",
-    features: [
-      "Users can add products to the watchlist by clicking on the extension icon on the product page.",
-      "Users can set a price alert by entering the desired price for the product.",
-    ],
-    tags: [
-      {
-        name: "JavaScript",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Chrome Extension",
-        color: "green-text-gradient",
-      },
-      {
-        name: "ReactJs",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tracker,
-    source_code_link: "https://github.com/himanshu8443/FlipKart-Price-Tracker",
-    live_link:
-      "https://chromewebstore.google.com/detail/fliptrack-flipkart-price/pekpfcghlbljghaojdlajgpceebobokd?hl=en",
-  },
-  {
-    name: "Social Sphere",
-    description:
-      "A full-stack social media application with user authentication, infinite scroll, and image and video uploads.",
-    features: [
-      "User authentication using JWT and OAuth providers.",
-      "Infinite scroll for posts.",
-      "Image and video uploads.",
-      "Like, comment, and share posts.",
-      "Follow and unfollow users.",
-      "Dark mode support.",
-    ],
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "nodejs",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Supabase",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "redux",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: socialSphere,
-    source_code_link: "https://github.com/himanshu8443/SocialSphere",
-    live_link: "https://socialspherex.vercel.app/",
   },
 ];
 
